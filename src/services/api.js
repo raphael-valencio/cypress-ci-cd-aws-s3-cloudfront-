1cca8de8d5bb115051ad7544ac21fb232e94cb28
Raphael Valencio Martinez <ph.valencio@gamil.com>
2024-09-01 12:28:03 -0400

    chore(api): adicionar cabeçalho 'Origin' por padrão em todas as requisições Axios

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_BASE_URL
+    baseURL: process.env.REACT_APP_API_BASE_URL,
+    headers: {
+        'Origin': process.env.REACT_APP_API_BASE_URL
+    }
 });
 
 export const registerUser = (userData) => {
