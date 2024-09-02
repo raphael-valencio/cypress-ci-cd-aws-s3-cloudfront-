e97a8395f4edf774ebcb4ea7b418973fe661c077
Raphael Valencio Martinez <ph.valencio@gamil.com>
2024-09-01 23:43:26 -0400

    chore(tests): configurar mochawesome como reporter no cypress.config.js
    - Configurado o mochawesome como reporter de testes no Cypress.
    - Definido diretório de relatórios como 'cypress/results'.
    - Relatórios em HTML ativados e JSON desativado.
    - Timestamp configurado no formato 'mmddyyyy_HHMMSS'.
    - Sobrescrita de arquivos desativada.

--- cypress.config.js
+++ cypress.config.js
@@ -4,4 +4,13 @@ module.exports = defineConfig({
   e2e: {
     baseUrl:"http://localhost:3000",
   },
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    overwrite: false,
+    html: true,
+    json: false,
+    timestamp: 'mmddyyyy_HHMMSS'
+  },
+
 });
